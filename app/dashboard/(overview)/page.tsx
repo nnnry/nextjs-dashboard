f7477c0c3fe41b181e0baec157612f0ece8059d3
nnnry <eren_piao@outlook.com>
2024-05-31 21:13:15 -0700

    Added search and pagination

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,9 +2,9 @@ import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import  { lusitana } from '@/app/ui/fonts'
-import { fetchCardData} from '../../lib/data';
-import { Suspense } from 'react';
 import {RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton} from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchCardData} from '../../lib/data';
 
 export default async function Page(){
     const {numberOfInvoices, numberOfCustomers, totalPaidInvoices, totalPendingInvoices} = await fetchCardData();
