1af5e25290cf5cefa651343ac50407a41eb8a977
nnnry <eren_piao@outlook.com>
2024-06-02 16:21:24 -0700

    Added accessibility practices and server-side form validation (2)

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -43,7 +43,9 @@ export async function createInvoice(prevState: State, formData: FormData) {
       message: 'Missing Fields. Failed to Create Invoice.',
     };
   }
-  
+
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
